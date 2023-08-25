900fad963259e2dc3420c7a86843a1adef39fa9a
Mohammad Khalooei <mkhalooei90@gmail.com>
2023-08-26 01:56:10 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='Robustness-Framework',
-    version='0.1.0',
+    version='0.0.3',
     author='Mohammad KHalooei',
+    url='https://github.com/khalooei/robustness-framework',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author_email='mkhalooei90@gmail.com',
     description='An efficient framework for establishing a baseline for standard and adversarial machine learning training projects ',
     packages=find_packages(),
